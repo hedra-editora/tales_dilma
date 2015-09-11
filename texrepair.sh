01a796246314ec97307b5103d3736c30c83990e7
Jorge Sallum <jorge@hedra.com.br>
2015-09-11 16:07:59 -0300

    versão do texto 2.0 com Tales do lado

--- texrepair.sh
+++ texrepair.sh
@@ -1,4 +1,4 @@
-#sed -i 's/[A-ZÁÉÍÓÚÀÇÃÕÂÊÔ][A-ZÁÉÍÓÚÀÇÃÕÂÊÔ]\+/\\versal{&}/g' *.tex
+sed -i 's/[A-ZÁÉÍÓÚÀÇÃÕÂÊÔ][A-ZÁÉÍÓÚÀÇÃÕÂÊÔ]\+/\\versal{&}/g' TEXTO_2.tex
 #sed -i 's/\\versa{\\versal/{\\versal/g' *.tex
 sed -i 's/\.\.\./…/g' *.tex
 sed -i 's/\ -\ /\ --\ /g' *.tex
